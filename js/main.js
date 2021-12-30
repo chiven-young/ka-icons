9b61dd59e32e5554bee4881f222f563406377fdf
chiven <chiven@yeah.net>
2021-12-30 22:41:02 +0800

    新增图标搜索功能

--- js/main.js
+++ js/main.js
@@ -12,6 +12,18 @@ var app = new Vue({
             updateTimeAll: "2021.12.12",
             icons: [],
             themes: [],
+            search: '',
+            searchData: [],
+        }
+    },
+    watch: {
+        // 监听input输入框，若没东西了，就回复默认状态
+        search:function(newnew,oldold){
+          if(newnew == ""){
+            // 发请求回到初始列表数据状态
+            console.log("搜索框没东西了，回复初始状态");
+            app.$data.searchData = app.$data.icons;
+          }
         }
     },
     methods: {
@@ -25,43 +37,67 @@ var app = new Vue({
                 }
             })
         },
+        getIconsData: function() {
+            axios
+            .get('../assets/icons.json')
+            .then(response => (this.icons = response.data.itemsList))
+            .catch(function (error) { // 请求失败处理
+                console.log(error);
+            });
+            axios
+            .get('../assets/themes.json')
+            .then(response => (this.themes = response.data.themes))
+            .catch(function (error) { // 请求失败处理
+                console.log(error);
+            });
+        },
+        Search: function() {
+            // search 是 v-model="search" 的 search
+            var search = this.search;
+            if (search) {
+                this.searchData = this.icons.filter(function(icon) {
+                    // 每一项数据
+                    // console.log(product)
+                    return Object.keys(icon).some(function(key) {
+                    // 每一项数据的参数名
+                    // console.log(key)
+                    return (
+                        String(icon[key])
+                        // toLowerCase() 方法用于把字符串转换为小写。
+                        .toLowerCase()
+                        // indexOf() 方法可返回某个指定的字符串值在字符串中首次出现的位置。
+                        .indexOf(search) > -1
+                    );
+                    });
+                });
+            }
+        }
     },
     beforeCreate: function () {
-        console.log('beforeCreate 创建前状态===============》');
+        // console.log('beforeCreate 创建前状态===============》');
     },
     created: function () {
         // this.getComments();
-        console.log('created 创建完毕状态===============》');
+        // console.log('created 创建完毕状态===============》');
     },
     beforeMount: function () {
-        console.log('beforeMount 挂载前状态===============》');
+        // console.log('beforeMount 挂载前状态===============》');
     },
     mounted: function () {
-        axios
-        .get('../assets/icons.json')
-        .then(response => (this.icons = response.data.itemsList))
-        .catch(function (error) { // 请求失败处理
-            console.log(error);
-        });
-        axios
-        .get('../assets/themes.json')
-        .then(response => (this.themes = response.data.themes))
-        .catch(function (error) { // 请求失败处理
-            console.log(error);
-        });
-        console.log('mounted 挂载结束状态===============》');
+        this.getIconsData();
+        // console.log('mounted 挂载结束状态===============》');
     },
     beforeUpdate: function () {
-        console.log('beforeUpdate 更新前状态===============》'); //这里指的是页面渲染新数据之前
+        // console.log('beforeUpdate 更新前状态===============》'); 
     },
     updated: function () {
-        console.log('updated 更新完成状态===============》');
+        // console.log('updated 更新完成状态===============》');
     },
     beforeDestroy: function () {
-        console.log('beforeDestroy 销毁前状态===============》');
+        // console.log('beforeDestroy 销毁前状态===============》');
     },
     destroyed: function () {
-        console.log('destroyed 销毁完成状态===============》');
+        // console.log('destroyed 销毁完成状态===============》');
     }
 })
 
